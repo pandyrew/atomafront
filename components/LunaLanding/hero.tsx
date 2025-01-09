0529e2e6c6315e1c34458d910afa70e496b9f1c0
Andrew Hwang <pandy430@gmail.com>
2025-01-09 15:01:50 -0800

    starting point for luna page hero

--- components/LunaLanding/hero.tsx
+++ components/LunaLanding/hero.tsx
@@ -3,14 +3,36 @@ import { AtomSVG } from "../Landing/AtomSVG";
 export const Hero = () => {
   return (
     <>
-      <section className="relative min-h-screen ">
+      <section className="relative min-h-screen flex items-center">
         {/* Gradient blob */}
-        <div className="absolute -top-[400px] translate-x-1/2 w-[800px] h-[800px] pointer-events-none">
-          <div className="absolute inset-0 bg-gradient-to-b from-blue-400 via-purple-400 to-[#ffc55b] blur-3xl opacity-10" />
+        <div className="absolute -top-[500px] translate-x-1/2 right-[50%] w-[800px] h-[800px] pointer-events-none rounded-full">
+          <div className="absolute inset-0 bg-gradient-to-r from-emerald-400 via-blue-400 to-indigo-400 blur-3xl opacity-10 rounded-full" />
+        </div>
+        <div className="absolute -top-[520px] left-0 w-full h-full opacity-10">
+          <AtomSVG isColored={false} />
         </div>
 
-        <div className="absolute -top-[500px] left-0 w-full h-full opacity-10">
-          <AtomSVG isColored={false} />
+        {/* Hero Content */}
+        <div className="container mx-auto px-4 mt-[30vh]">
+          <div className="space-y-6">
+            <h1 className="font-outfit text-6xl font-bold bg-gradient-to-r from-emerald-400 to-blue-500 bg-clip-text text-transparent">
+              Atoma Luna
+            </h1>
+
+            {/* Horizontal Decorative Line */}
+            <div className="w-full h-px bg-gray-700" />
+            <div className="flex flex-col space-y-2">
+              <h2 className="font-outfit text-4xl font-light text-gray-200">
+                Your AI-Powered
+                <br />
+                <span className="font-semibold">Financial Insights Engine</span>
+              </h2>
+              <p className="text-gray-400 font-outfit">
+                SEC filings summarized. Market sentiment analyzed. Financial
+                news decoded.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
